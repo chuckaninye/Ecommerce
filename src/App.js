1ce1b1eed7182c2d4a35ad1bd5e003542589ccbe
chuckaninye <chuckaninye@gmail.com>
2021-03-09 19:55:20 -0500

    Implemented react routing and added footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import React from "react";
-import "./default.scss";
-import Header from "./components/Header";
+import { Switch, Route } from "react-router-dom";
+
+//layout
+import MainLayout from "./layouts/MainLayout";
+import HomepageLayout from "./layouts/HomepageLayout";
+
+//pages
 import Homepage from "./pages/Homepage";
+import Registration from "./pages/Registration";
+import "./default.scss";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="main">
-        <Homepage />
-      </div>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <HomepageLayout>
+              <Homepage />
+            </HomepageLayout>
+          )}
+        />
+        <Route
+          exact
+          path="/registration"
+          render={() => (
+            <MainLayout>
+              <Registration />
+            </MainLayout>
+          )}
+        />
+      </Switch>
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import './styles.scss';
-import Logo from '../../assets/logo.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles.scss";
+import Logo from "../../assets/logo.png";
 
-const Header = props => {
-    return (
-        <header className="header">
-            <div className="wrap">
-                <div className="logo">
-                    <img src={Logo} alt="SimpleTut LOGO" />
-                </div>
-            </div>
-        </header>
-    )
-}
+const Header = (props) => {
+  return (
+    <header className="header">
+      <div className="wrap">
+        <div className="logo">
+          <Link to="/">
+            <img src={Logo} alt="SimpleTut LOGO" />
+          </Link>
+        </div>
+        <div className="callToActions">
+            <ul>
+                <li>
+                    <Link to="/registration"> Register</Link>
+                </li>
+            </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
